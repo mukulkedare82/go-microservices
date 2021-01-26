74d6be0b6fd694c9a41de282b60fde85a91d55c6
mukulkedare82 <mukulgit82@gmail.com>
2021-01-26 18:11:15 +0530

    server settings -and gracefull shutdown code added

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/mukulkedare/go-microservice-tuts/handlers"
 )
@@ -21,5 +24,39 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	//http.ListenAndServe(":9090", sm)
+
+	// create http server
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// listen is blocking call, so wrap it in a go func/routine
+	// this will not block in main and we can handle shutdown next
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	// register for signal
+	// create a channel to notify
+	// signal broadcasts to this channel whenever sigterm or any interrupt occurs
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan // blocks here
+	logger.Println("Received terminate, graceful shutdown", sig)
+
+	// gracefull shutdown
+	// close clients gracefully, wait for all incomplete requests to finish
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+
 }
