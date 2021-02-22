6a8f95f210709c4b18678768cee8c8e5d6d6e96a
mukulkedare82 <mukulgit82@gmail.com>
2021-02-22 19:06:44 +0530

    CORS changes

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mukulkedare/go-microservice-tuts/handlers"
 )
@@ -47,10 +48,34 @@ func main() {
 	// serving it from current working directory (http app directory)
 	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	// CORS - CROSS ORIGIN REQUESTS
+	// It is the mechanism of securing cross origin requests and data transfers
+	// between browser and servers. Browser by default blocks cross origin requests.
+	// Ex:
+	// 1. user logs in to ebank.com, it writes a cookie to browser
+	// 2. user visits some malicious website evil.com
+	// 3. evil.com makes call to ebank.com (malicious logic)
+	// 4. browser checks cookies for ebank.com and forwards it in the request (by evil.com)
+	//    to ebank.com server
+	// 5. evil.com could be doing some malicious activity in the request (like initiate a bank transfer)
+	//    it could possibly do it as browser is sending all cookies of ebank.com in request
+	//    to the server.
+	// 6. To avoid this situation CORS mechanism comes into play, before sending cookies to the server
+	//    CORS mechanism checks with the server "which origin requests it allows ?"
+	// 7. The server could say only "frontend.ebank.com", in the case evil.com request is rejected
+	//    as it is not the approved access control origin.
+	// 8. We can use * to give access to all origins, can be used for public API
+	// https://medium.com/@baphemot/understanding-cors-18ad6b478e2b
+
+	// To add CORS we can use gorilla middleware CORS
+	origins := gohandlers.AllowedOrigins([]string{"http://localhost:3000"})
+	//origins := gohandlers.AllowedOrigins([]string{"*"})
+	ch := gohandlers.CORS(origins)
+
 	// create http server
 	s := &http.Server{
 		Addr:         ":9090",
-		Handler:      sm,
+		Handler:      ch(sm),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
